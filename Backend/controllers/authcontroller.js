64af57b1994e4011d69d23eb31eba92ab7f4265f
Debesh Dinda <debeshdinda993@gmail.com>
2026-01-09 18:02:56 +0530

    updated cors and some cookie code

--- Backend/controllers/authcontroller.js
+++ Backend/controllers/authcontroller.js
@@ -38,7 +38,7 @@ const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,        
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "none",
             maxAge: 1 * 24 * 60 * 60 * 1000, 
         });
 
@@ -97,7 +97,7 @@ const signup = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "none",
             maxAge: 1 * 24 * 60 * 60 * 1000
         });
 
@@ -125,7 +125,7 @@ const logout = (req, res) => {
         res.clearCookie("token", {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict"
+            sameSite: "none"
         });
 
         return res.status(200).json({
