8a9f56c19a656fec9e3f6c7e9010869d6d6e24d6
Debesh Dinda <debeshdinda993@gmail.com>
2026-01-13 15:21:04 +0530

    code  updated

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -195,7 +195,7 @@ const Dashboard = () => {
         </h2>
         <button
           onClick={logout}
-          className="bg-red-500 hover:bg-red-600 text-white px-4 py-1 rounded-md"
+          className="bg-red-500 hover:bg-red-600 text-white px-2 py-1 rounded-md"
         >
           Logout
         </button>
